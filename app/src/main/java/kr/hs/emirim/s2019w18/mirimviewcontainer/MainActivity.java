5d285715bedddf596a91270ff06e7dbf339125d4
dayeon <dayeon2399@naver.com>
2020-11-05 19:57:28 +0900

    dayeon's commit - done:)

--- app/src/main/java/kr/hs/emirim/s2019w18/mirimviewcontainer/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w18/mirimviewcontainer/MainActivity.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.s2019w18.mirimviewcontainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ViewFlipper;
@@ -18,7 +19,30 @@ public class MainActivity extends AppCompatActivity {
         Button btnNext=findViewById(R.id.btn_stop);
         btnPrev.setOnClickListener(btnListener);
         btnNext.setOnClickListener(btnListener);
+        flipper.setOnTouchListener(touchListener);
     }
+
+    float downX,upX;
+    View.OnTouchListener touchListener = new View.OnTouchListener(){
+        @Override
+        public boolean onTouch(View view, MotionEvent e) {
+            if(e.getAction()==MotionEvent.ACTION_DOWN){//오른쪽에서 왼쪽으로 터치
+                downX=e.getX();
+
+            }else if(e.getAction()==MotionEvent.ACTION_UP){//눌르고 떼기
+                upX=e.getX();
+
+                if(downX>upX){
+                    flipper.showNext();
+                }else if(downX<upX){
+                    flipper.showPrevious();
+                }
+            }
+
+
+            return false;
+        }
+    };
 
     View.OnClickListener btnListener=new View.OnClickListener(){
         @Override
